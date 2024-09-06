51b73e2755d5b55a97e6c37aec3be59d3534a83d
tal-rofe <talrofe111+1@gmail.com>
2024-09-06 20:37:21 +0300

    fix: 🐞 fix no NPM publish in release config

--- release.config.cjs
+++ release.config.cjs
@@ -18,7 +18,7 @@ const config = {
 		'@semantic-release/changelog',
 		'@semantic-release/git',
 		'@semantic-release/github',
-		['@semantic-release/npm', { npmPublish: false }],
+		['@semantic-release/npm', { npmPublish: true }],
 	],
 };
 
