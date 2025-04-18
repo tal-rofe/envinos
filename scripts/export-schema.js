abd50933372e910cb4a92fcd9cfb4675b0f67032
tal-rofe <talrofe111@gmail.com>
2025-04-18 10:20:35 +0300

    refactor: 💡 fix Prettier

--- scripts/export-schema.js
+++ scripts/export-schema.js
@@ -1,21 +1,21 @@
-import fs from "node:fs/promises";
-import path from "node:path";
+import fs from 'node:fs/promises';
+import path from 'node:path';
 
 import { load as tsImportLoad } from 'ts-import';
-import { z } from "zod";
+import { z } from 'zod';
 
 const schemaPath = path.join(process.cwd(), 'src', 'schemas', 'configuration.ts');
 const schemaModule = await tsImportLoad(schemaPath, { useCache: false });
 const zodSchema = schemaModule.configurationSchema;
-const jsonSchema = z.toJSONSchema(zodSchema, { target: "draft-7" });
+const jsonSchema = z.toJSONSchema(zodSchema, { target: 'draft-7' });
 
 const fullJsonSchema = {
-	"$schema": "http://json-schema.org/draft-07/schema#",
-	"$ref": "#/definitions/envinos",
-	"title": "Envinos configuration for JSON",
-	"definitions": {
-		"envinos": jsonSchema
-	}
-}
+	$schema: 'http://json-schema.org/draft-07/schema#',
+	$ref: '#/definitions/envinos',
+	title: 'Envinos configuration for JSON',
+	definitions: {
+		envinos: jsonSchema,
+	},
+};
 
 await fs.writeFile(path.join(process.cwd(), 'schema.json'), JSON.stringify(fullJsonSchema, null, 2));
